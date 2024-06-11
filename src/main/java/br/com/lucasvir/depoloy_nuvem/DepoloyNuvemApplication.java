34cbabae7b85fb7fe16ca44a3ebf3d11113d88ce
lucasvir <lavirmond@gmail.com>
2024-06-11 16:51:00 -0300

    init :)

---
+++ src/main/java/br/com/lucasvir/depoloy_nuvem/DepoloyNuvemApplication.java
@@ -0,0 +1,12 @@
+package br.com.lucasvir.depoloy_nuvem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepoloyNuvemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepoloyNuvemApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/lucasvir/depoloy_nuvem/DepoloyNuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucasvir.depoloy_nuvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepoloyNuvemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
